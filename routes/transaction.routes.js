396293b473d947c1789c7eea2a140eba48160311
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-03-03 13:47:28 +0000

    Filter function

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,17 +1,11 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/transaction.controller");
 
-
-
-
 router.get("/ajoute", controller.test);
 
-router.post("/seed",controller.ajouter);
+router.post("/seed", controller.ajouter);
 
-router.get("/:type", controller.filterByType)
-
-
+router.get("/filter", controller.filterByType);
 
 module.exports = router;
