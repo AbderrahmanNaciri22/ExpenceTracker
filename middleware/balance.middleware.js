d4bde761fca3cafbbe02ddbc7c8e95b10e2bc4c8
AbderrahmanNaciri22 <abderrhmanen2205@gmail.com>
2026-03-04 11:16:07 +0000

    compelet balance middleware

--- middleware/balance.middleware.js
+++ middleware/balance.middleware.js
@@ -1,9 +1,33 @@
 const Transaction = require("../utils/export.util");
-
 const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
 
+async function getTotalExpense(type) {
+    const transactions = await Transaction.find({ type: type }).sort({ date: -1 });
+
+    let total = 0;
+
+    transactions.forEach(t => {
+        total += t.amount;
+    });
+
+    return total;
+}
+
+
 async function blanceVerifyAndCategoryCheck(req, res, next) {
+         let totalExpense = await getTotalExpense("expense");
+         let totalIncome = await getTotalExpense("income");
+
+         blance = totalIncome-totalExpense;
+
+         console.log(blance);
         const {title,amount,type,category,date,} = req.body;
+        if(type == "expense"){
+        if(blance<amount){
+            return res.status(404).json("Balance not enough")
+        };
+        }
+
         if(!title){
             return res.send("title vide")
         }
@@ -13,6 +37,7 @@ async function blanceVerifyAndCategoryCheck(req, res, next) {
         if (!dateRegex.test(date)) {
             return res.status(400).json({ message: "Date must be yyyy/mm/dd" });
             }
+
       next()
 }
 
